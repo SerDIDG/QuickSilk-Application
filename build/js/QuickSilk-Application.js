ca607d1194f03196a65b72bf792b4624d604d0f0
Serhio Magpie <serdidg@gmail.com>
2016-08-26 17:45:00 +0300

    JS > App.FileUploaderContainer: added Request.

--- build/js/QuickSilk-Application.js
+++ build/js/QuickSilk-Application.js
@@ -1,11 +1,11 @@
-/*! ************ QuickSilk-Application v3.14.1 (2016-08-25 21:16) ************ */
+/*! ************ QuickSilk-Application v3.14.2 (2016-08-26 17:42) ************ */
 
 // /* ************************************************ */
 // /* ******* QUICKSILK: COMMON ******* */
 // /* ************************************************ */
 
 var App = {
-    '_version' : '3.14.1',
+    '_version' : '3.14.2',
     'Elements': {},
     'Nodes' : {},
     'Test' : []
@@ -1940,6 +1940,7 @@ function(params){
 cm.define('App.FileUploaderContainer', {
     'extend' : 'Com.FileUploaderContainer',
     'params' : {
+        'action' : '',
         'constructor' : 'App.FileUploader'
     }
 },
@@ -1947,6 +1948,44 @@ function(params){
     var that = this;
     // Call parent class construct
     Com.FileUploaderContainer.apply(that, arguments);
+});
+
+
+cm.getConstructor('App.FileUploaderContainer', function(classConstructor, className, classProto){
+    var _inherit = classProto._inherit;
+
+    classProto.construct = function(){
+        var that = this;
+        // Variables
+        that.isRequest = false;
+        // Call parent method
+        _inherit.prototype.construct.apply(that, arguments);
+        return that;
+    };
+
+    classProto.validateParams = function(){
+        var that = this;
+        // Call parent method
+        _inherit.prototype.validateParams.apply(that, arguments);
+        // Validate Request
+        that.isRequest = !cm.isEmpty(that.params['action']) && window.Request && window.Request.send;
+    };
+
+    classProto.renderControllerEvents = function(){
+        var that = this;
+        // Call parent method
+        _inherit.prototype.renderControllerEvents.apply(that, arguments);
+        // Add Request Event
+        if(that.isRequest){
+            that.components['controller'].addEvent('onComplete', function(my, data){
+                Request.send(that.params['action'], data, 'post', {
+                    'node' : that.params['node'],
+                    'controller' : that
+                });
+            });
+        }
+        return that;
+    };
 });
 cm.define('App.FileUploaderLocal', {
     'extend' : 'Com.FileUploaderLocal',
--- src/js/components/FileUploaderContainer.js
+++ src/js/components/FileUploaderContainer.js
@@ -1,6 +1,7 @@
 cm.define('App.FileUploaderContainer', {
     'extend' : 'Com.FileUploaderContainer',
     'params' : {
+        'action' : '',
         'constructor' : 'App.FileUploader'
     }
 },
@@ -8,4 +9,42 @@ function(params){
     var that = this;
     // Call parent class construct
     Com.FileUploaderContainer.apply(that, arguments);
+});
+
+
+cm.getConstructor('App.FileUploaderContainer', function(classConstructor, className, classProto){
+    var _inherit = classProto._inherit;
+
+    classProto.construct = function(){
+        var that = this;
+        // Variables
+        that.isRequest = false;
+        // Call parent method
+        _inherit.prototype.construct.apply(that, arguments);
+        return that;
+    };
+
+    classProto.validateParams = function(){
+        var that = this;
+        // Call parent method
+        _inherit.prototype.validateParams.apply(that, arguments);
+        // Validate Request
+        that.isRequest = !cm.isEmpty(that.params['action']) && window.Request && window.Request.send;
+    };
+
+    classProto.renderControllerEvents = function(){
+        var that = this;
+        // Call parent method
+        _inherit.prototype.renderControllerEvents.apply(that, arguments);
+        // Add Request Event
+        if(that.isRequest){
+            that.components['controller'].addEvent('onComplete', function(my, data){
+                Request.send(that.params['action'], data, 'post', {
+                    'node' : that.params['node'],
+                    'controller' : that
+                });
+            });
+        }
+        return that;
+    };
 });
