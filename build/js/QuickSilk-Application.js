a4a491f6c04f8af0015d253cc645cc5f05270706
Serhio Magpie <serdidg@gmail.com>
2017-02-22 19:03:55 +0200

    Form Styles renew

--- build/js/QuickSilk-Application.js
+++ build/js/QuickSilk-Application.js
@@ -1,11 +1,11 @@
-/*! ************ QuickSilk-Application v3.15.19 (2017-02-20 19:39) ************ */
+/*! ************ QuickSilk-Application v3.15.20 (2017-02-22 19:03) ************ */
 
 // /* ************************************************ */
 // /* ******* QUICKSILK: COMMON ******* */
 // /* ************************************************ */
 
 var App = {
-    '_version' : '3.15.19',
+    '_version' : '3.15.20',
     'Elements': {},
     'Nodes' : {},
     'Test' : []
@@ -2606,54 +2606,6 @@ function(params){
 cm.getConstructor('App.FormStyles', function(classConstructor, className, classProto){
     var _inherit = classProto._inherit;
 
-    classProto.onConstruct = function(){
-        var that = this;
-        // Bind context to methods
-        that.outerIndentTriggerHandler = that.outerIndentTrigger.bind(that);
-        that.backgroundPositionTriggerHandler = that.backgroundPositionTrigger.bind(that);
-    };
-
-    classProto.onRender = function(){
-        var that = this;
-        that.outerIndentTrigger();
-        that.backgroundPositionTrigger();
-    };
-
-    classProto.renderViewModel = function(){
-        var that = this;
-        // Find Components
-        cm.find('Com.BoxTools', 'outer-indent', that.nodes['container'], function(classObject){
-            that.components['outer-indent'] = classObject;
-        });
-        cm.find('Com.PositionTools', 'background-position', that.nodes['container'], function(classObject){
-            that.components['background-position'] = classObject;
-        });
-        // Outer Indent Trigger
-        cm.addEvent(that.nodes['outer-indent-auto'], 'change', that.outerIndentTriggerHandler);
-        // Background Position Trigger
-        cm.addEvent(that.nodes['background-position-custom'], 'change', that.backgroundPositionTriggerHandler);
-    };
-
-    classProto.outerIndentTrigger = function(){
-        var that = this;
-        if(that.nodes['outer-indent-auto'].checked){
-            that.components['outer-indent'].disable();
-        }else{
-            that.components['outer-indent'].enable();
-        }
-    };
-
-    classProto.backgroundPositionTrigger = function(e){
-        var that = this;
-        if(that.nodes['background-position-custom'].checked){
-            that.components['background-position'].disable();
-            that.nodes['background-position-value'].disabled = false;
-            e && that.nodes['background-position-value'].focus();
-        }else{
-            that.components['background-position'].enable();
-            that.nodes['background-position-value'].disabled = true;
-        }
-    };
 });
 cm.define('App.HelpTour', {
     'modules' : [
